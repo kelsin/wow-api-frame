89623c7d3179a84039d397a237b6ab944b681ff4
Christopher Giroir <kelsin@valefor.com>
2011-11-17 22:13:37 -0500

    Moving over to HttpClient which closes #1

--- src/main/java/com/valefor/wowapiframe/model/RealmList.java
+++ src/main/java/com/valefor/wowapiframe/model/RealmList.java
@@ -8,12 +8,20 @@ import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpEntity;
+import org.apache.http.util.EntityUtils;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.DefaultHttpClient;
+
 import com.google.gson.Gson;
 import com.google.gson.JsonParser;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonParser;
 import com.google.gson.JsonElement;
 
+import org.apache.http.client.methods.HttpGet;
 import com.valefor.wowapiframe.model.Realm;
 
 public class RealmList {
@@ -25,20 +33,30 @@ public class RealmList {
 
     // Instantiate a new realm list and populate it from the wow api
     public void init() {
-        this.realms = new Vector<Realm>();
-        StringBuffer json = new StringBuffer();
+        // Load from the json
+        this.realms = this.loadFromJson(this.getRealmJson());
+    }
+
+    private String getRealmJson() {
+        String json = "";
 
         try {
-            URL url = new URL("http://us.battle.net/api/wow/realm/status");
-
-            BufferedReader buff = new BufferedReader(new InputStreamReader((InputStream) url.getContent()));
-            String line = buff.readLine();
-            while (line != null) {
-                json.append(line + "\n");
-                line = buff.readLine();
+            HttpGet request = new HttpGet("http://us.battle.net/api/wow/realm/status");
+            HttpClient client = new DefaultHttpClient();
+            HttpResponse response = client.execute(request);
+            HttpEntity entity = response.getEntity();
+            if(entity != null) {
+                json = EntityUtils.toString(entity);
             }
         } catch (Exception e) {
+            // Oh well... no realms for now
         }
+
+        return json;
+    }
+
+    private List<Realm> loadFromJson(String json) {
+        List<Realm> realms = new Vector<Realm>();
 
         Gson gson = new Gson();
         JsonParser parser = new JsonParser();
@@ -47,9 +65,11 @@ public class RealmList {
         Iterator<JsonElement> i = array.iterator();
         while(i.hasNext()) {
             Realm realm = gson.fromJson(i.next(), Realm.class);
-            this.realms.add(realm);
+            realms.add(realm);
         }
 
-        System.out.println("Loaded " + this.size() + " realms");
+        System.out.println("Loaded " + realms.size() + " realms");
+
+        return realms;
     }
 }
